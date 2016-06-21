4e84f2f2f6020fa101e2cc438bc24320c4e47f15
= <=>
2016-06-21 17:44:12 +0800

    recyclerview adapter

---
+++ framework/src/main/java/com/Jsu/framework/recyclerview/anim/BaseAnimation.java
@@ -0,0 +1,13 @@
+package com.Jsu.framework.recyclerview.anim;
+
+import android.animation.Animator;
+import android.view.View;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public interface  BaseAnimation {
+
+    Animator[] getAnimators(View view);
+
+}
